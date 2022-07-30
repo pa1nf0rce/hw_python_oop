a72e6a02a7b0ec41c1b5085422a5fb0ac62cc3be
Роман Авраменко <roma.avramencko2012@yandex.ru>
2022-07-30 05:57:17 +0300

    Second attempt

--- homework.py
+++ homework.py
@@ -17,21 +17,21 @@ class InfoMessage:
                            self.duration,
                            self.distance,
                            self.speed,
-                           self.calories)                           
-    
+                           self.calories)                        
+
     def get_message(self) -> str:
 
         return ('Тип тренировки: {0}; '
                 'Длительность: {1:.3f} ч.; '
-                'Дистанция: {2:.3f} км; ' 
+                'Дистанция: {2:.3f} км; '
                 'Ср. скорость: {3:.3f} км/ч; '
                 'Потрачено ккал: {4:.3f}.').format(self.training_type,
                                                    self.duration,
                                                    self.distance,
                                                    self.speed,
                                                    self.calories)
-        
-        
+
+
 class Training:
     """Базовый класс тренировки."""
 
@@ -74,24 +74,25 @@ class Training:
 class Running(Training):
     """Тренировка: бег."""
 
-    def __init__(self, 
-                action: int, 
-                duration: float,
-                weight: float) -> None:
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float) -> None:
 
         super().__init__(action, duration, weight)
 
     def get_spent_calories(self) -> float:
-        """Формула расчета ср. скорости 
-        при занятии бегом:
-        (18 * средняя_скорость - 20) * вес_спортсмена / 
+        """Формула расчета ср.скорости при занятии бегом:
+        (18 * средняя_скорость - 20) * вес_спортсмена /
         M_IN_KM * время_тренировки_в_минутах.
         """
         times_in_minutes = self.duration * 60
         coeff_calorie_1: int = 18
         coeff_calorie_2: int = 20
-        return ((coeff_calorie_1 * self.get_mean_speed() - coeff_calorie_2) 
-               * self.weight / self.M_IN_KM * times_in_minutes)
+        return (
+            (coeff_calorie_1 * self.get_mean_speed() - coeff_calorie_2) 
+            * self.weight / self.M_IN_KM * times_in_minutes
+            )
 
 
 class SportsWalking(Training):
@@ -117,9 +118,11 @@ class SportsWalking(Training):
         time_in_minutes: float = self.duration * 60
         coeff_calorie_1: float = 0.035
         coeff_calorie_2: float = 0.029
-        return ((coeff_calorie_1 * self.weight + 
-               (self.get_mean_speed() ** 2 // self.height) 
-               * coeff_calorie_2 * self.weight) * time_in_minutes)
+        return (
+            (coeff_calorie_1 * self.weight + 
+            (self.get_mean_speed() ** 2 // self.height) 
+            * coeff_calorie_2 * self.weight) * time_in_minutes
+               )
 
 
 class Swimming(Training):
@@ -174,7 +177,7 @@ def main(training: Training) -> None:
     """Главная функция."""
     info = training.show_training_info()
     print(info.get_message())
-    
+
 if __name__ == '__main__':
     packages: List[Tuple[str,List[int]]]
 
